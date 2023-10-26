3e7d48799366d1f875132e0b40606c63fc011779
Arturo Puentes <arturo.puentes.yu@gmail.com>
2023-10-26 14:13:31 +0200

    Arreglos de detalles

--- php/contacto.php
+++ php/contacto.php
@@ -7,10 +7,16 @@
     <title>Contactanos</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
     <link rel="stylesheet" href="../styles.css">
 </head>
 
 <body>
+    <?php
+    require_once "secciones.php";
+    CrearSecciones::crearNav(1);
+    ?>
     <main id="contactanos">
         <div class="container my-5 px-5">
             <h2 class="text-center">Contáctanos!</h2>
@@ -28,10 +34,7 @@
         </div>
     </main>
 
-    <nav class=""></nav>
-
     <?php
-    require_once "secciones.php";
     CrearSecciones::crearFooter(1);
     ?>
 </body>
